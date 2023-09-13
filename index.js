dd70e5676b1600d08279924d13cac7f6f657de0d
Sparkyu222 <sparkyu222@gmail.com>
2023-09-13 22:55:15 +0400

    made a little bit of cleanup between files and useless comments

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 import { WebSocketServer } from 'ws';                               // Pour les WebSockets
 import chalk from 'chalk';                                          // Pour print en couleur dans la console
 import ical from 'node-ical';                                       // Pour parser les fichiers ICS/iCAL
-import { downloadCalendar } from './src/downloader.js';             // Fonctions pour parser des éléments dans un évênement
-import * as parser from './src/ics-parser.js';
+import { downloadCalendar } from './src/downloader.js';             // Fonctions pour télécharger le calendrier
+import * as parser from './src/ics-parser.js';                      // Fonctions pour parser des éléments dans un évênement
+import * as chokidar from 'chokidar';                               // Librairie pour watch un dossier et ses sous-dossiers
+import { sleep } from './src/utils.js';
 
+// On démarre le WebSocket sur le port 8080
 const wss = new WebSocketServer({ port: 8080 });
 
 // Objet de réponse
 var objReponse = {
-    type: "calendar",
-    error: false,
-    content: {}
+    type: "",
+    error: null,
+    content: null
 }
 
+// Initiation des fonctions d'écoute de sockets
 wss.on('connection', (ws) => {  
     console.log(chalk.yellow(`Un client s'est connecté`));
 
+    // Quand le Websocket reçoit un message
     ws.on('message', async (data) => {
         console.log(`Message reçu : ${data}`);
     });
@@ -42,6 +47,8 @@ wss.on('connection', (ws) => {
 
         var sendObj = objReponse;
 
+        sendObj.tyoe = "calendar";
+        sendObj.error = false;
         sendObj.content = obj;
 
         ws.send(JSON.stringify(sendObj));
@@ -50,77 +57,14 @@ wss.on('connection', (ws) => {
 
 });
 
-/*
+var watchedPath = chokidar.watch('./data/').on('all', (event, path) => {
+    
+});
+
 (async () => {
-    const icsFile = await downloadCalendar();
-    const parsedICS = ical.sync.parseICS(icsFile);
+    await sleep(5000);
+    console.log(watchedPath.getWatched());
 
-    var obj = {};
-
-    for (const event of Object.keys(parsedICS)) {
-        if ((event) === 'vcalendar') continue;
-        let description = parser.extractDesc(parsedICS[event].description);
-        obj[event] = {
-            subject: parsedICS[event].summary,
-            location: parsedICS[event].location,
-            start_time: parsedICS[event].start,
-            duration: parsedICS[event].end.getHours() - parsedICS[event].start.getHours(),
-            teacher: description.teacher,
-            groups: description.groups,
-            exam: description.exam
-        }
-    }
-    console.log(obj);
 })();
 
-var directory2 = {
-    calendar: {
-        name: "Calendrier",
-        type: "folder",
-        content: {
-
-                item1: {
-                    name: "RT1",
-                    type: "folder",
-                    content: {},
-                },
-                item2: {
-                    name: "RT2",
-                    type: "folder",
-                    content: {
-                        folder21: {
-                            name: "RT2",
-                            type: "folder",
-                            content: {
-                                file1: {
-                                    name: "TP1",
-                                    type: "calendar",
-                                    uid: 4096
-                                },
-                                file2: {
-                                    name: "notes",
-                                    type: "pdf",
-                                    uid
-                                }
-                            }
-                        }
-                    }
-                }
-
-        }
-
-    }
-}
-
-var obj = {
-    item1: {
-        item21: true,
-        item22: false,
-        item23: null
-    },
-    item2: false,
-    item3: null
-}
-
-*/
 console.log('Le serveur a été initialisé');
---
+++ src/path-parser.js
@@ -0,0 +1,10 @@
+function makePath(watcherPath) {
+    var path = {},                                                  // Path final
+        slicedFolders = [];                                         // Dossiers et sous-dossiers récupérés
+    let i=0;
+    for (const path of Object.keys(watcherPath)) {
+        if (i==0) continue;
+        
+        i++;
+    }
+}
